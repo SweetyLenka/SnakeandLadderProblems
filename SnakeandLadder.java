55ce5de454d0b28370803550c1595cacade8f0bc
sweetylenka <sweetylenka2013@gmail.com>
2021-03-24 14:59:57 +0530

    The Player then checks for a Option. They are No Play, Ladder or Snake. - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the same position - In Case of Ladder the player moves ahead by the number of position received in the die
    - In Case of Snake the player moves behind

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -7,9 +7,29 @@ public class SnakeandLadder {
 		System.out.println("Start Position of Player : "+position);
 		//Random Function to generate values between a range
 		//Formula = Math.random() * (max - min + 1) + min
-		double Dice = Math.floor(Math.random()*(6-1+1)+1);
+		int Dice = (int) Math.floor(Math.random()*(6-1+1)+1);
 		System.out.println("Number on Dice : "+Dice);
+		
+		//Options
+		int Option = (int) Math.floor(Math.random()*10)%3;
+		
+		System.out.println("====OPTIONS======\nOption 0 : No Play\nOption 1 : Ladder \nOption 2 : Snake \n");
+		System.out.println("Option : "+Option);
+		switch(Option) {
+		case 1:
+			position += Dice;
+			System.out.println("Player moves ahead by : "+position);
+			break;
+		case 2:
+			position -= Dice;
+			System.out.println("Player moves behind by : "+position);
+			break;
+		default:
+			System.out.println("Player stays in the same position : "+position);
+			
+		
 		}
+	}
 }
 
 
