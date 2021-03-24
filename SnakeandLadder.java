0b1dfab1e872444de61993dae953e6442383ab97
sweetylenka <sweetylenka2013@gmail.com>
2021-03-24 16:11:17 +0530

    Repeat till the Player reaches the winning position 100. - Note In case the player position moves below 0, then the player restarts from 0

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -4,32 +4,61 @@ public class SnakeandLadder {
 	{
 		System.out.println("WECOME TO SNAKE AND LADDER PROBLEMS");
 		int position=0;
-		System.out.println("Start Position of Player : "+position);
-		//Random Function to generate values between a range
-		//Formula = Math.random() * (max - min + 1) + min
-		int Dice = (int) Math.floor(Math.random()*(6-1+1)+1);
-		System.out.println("Number on Dice : "+Dice);
+		System.out.println("Initial Position of Player : "+position);
 		
-		//Options
-		int Option = (int) Math.floor(Math.random()*10)%3;
 		
-		System.out.println("====OPTIONS======\nOption 0 : No Play\nOption 1 : Ladder \nOption 2 : Snake \n");
-		System.out.println("Option : "+Option);
-		switch(Option) {
-		case 1:
-			position += Dice;
-			System.out.println("Player moves ahead by : "+position);
-			break;
-		case 2:
-			position -= Dice;
-			System.out.println("Player moves behind by : "+position);
-			break;
-		default:
-			System.out.println("Player stays in the same position : "+position);
+		while(position<100)
+		{
+			//Random Function to generate values between a range
+			//Formula = Math.random() * (max - min + 1) + min
+			int Dice = (int) Math.floor(Math.random()*(6-1+1)+1);
+			System.out.println("Number on Dice : "+Dice);
+			int Option = (int) Math.floor(Math.random()*10)%3;
 			
+			System.out.println("====OPTIONS======\nOption 0 : No Play\nOption 1 : Ladder \nOption 2 : Snake \n");
+			System.out.println("Option : "+Option);
+			
+			if (Option == 0)
+			{
+				System.out.println("No Play the player stays in the same position "+position);
+			}
+			else if (Option == 1)
+			{
+				position = position + Dice;
+				//System.out.println(position);
+				if(position > 100)
+				{
+					position=100;
+					System.out.println("Player has won : "+position);
+				}
+				else if (position == 100)
+				{
+					System.out.println("Player has won : "+position);
+				}
+				else
+				{
+					System.out.println("Player moves ahead by : "+position);
+				}
+					
+					
+				
+			}
+			else 
+			{
+				position = position - Dice;
+				System.out.println("Player moves behind by :" +position);
+				if(position < 0)
+				{
+					position = 0;
+					System.out.println("Player restarts from " +position);
+				}
+				
+			}
+
 		
 		}
+		
+	}		
 	}
-}
 
 
