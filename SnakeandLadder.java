46b8a4a8e029ffa449df2e6522aed988e5dab48f
sweetylenka <sweetylenka2013@gmail.com>
2021-03-24 14:03:06 +0530

    The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number between 1 to 6

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -5,6 +5,10 @@ public class SnakeandLadder {
 		System.out.println("WECOME TO SNAKE AND LADDER PROBLEMS");
 		int position=0;
 		System.out.println("Start Position of Player : "+position);
+		//Random Function to generate values between a range
+		//Formula = Math.random() * (max - min + 1) + min
+		double Dice = Math.floor(Math.random()*(6-1+1)+1);
+		System.out.println("Number on Dice : "+Dice);
 		}
 }
 
